303f10725e7a11acf13109bf1d25fe74ff036b26
AMW - Game & Entertainment <animemixedworld@gmail.com>
2023-02-19 00:48:44 +0100

    feat: Added box list

---
+++ src/app/modules/games/games.component.spec.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { GamesComponent } from './games.component';
+
+describe('GamesComponent', () => {
+});
